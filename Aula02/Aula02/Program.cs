4087d2f03bda737374639630233c996a7c51bad5
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:35:31 -0300

    add: Aula 2 do curso

---
+++ Aula02/Aula02/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula02;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Console.WriteLine("Testando");
+
+        string myName = "Regina";
+        int idade = 22;
+        double peso = 10.5;
+        char character = '4';
+        bool boolean = true;
+
+        Console.WriteLine(myName);
+        Console.WriteLine(idade);
+        Console.WriteLine(peso);
+        Console.WriteLine(character);
+        Console.WriteLine(boolean);
+    }
+}
