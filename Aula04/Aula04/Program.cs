f1aacd6a23d0946963c7266ff86db8a1c00a2097
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:37:32 -0300

    add: Aula 4 do curso

---
+++ Aula04/Aula04/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("---------CADASTRO DE USUÁRIOS----------");
+
+        //Solicitar o nome do usuário
+        Console.WriteLine("Digite seu nome:");
+        string name = Console.ReadLine();
+        
+        //Solicitar a idade do usuário
+        Console.WriteLine("Digite a sua idade:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("-----------------------------------");
+
+        //Saída de dados
+        Console.WriteLine("O nome do usuário é: " + name);
+        Console.WriteLine("A idade do usuário é: " + age);
+    }
+}
