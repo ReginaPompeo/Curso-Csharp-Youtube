f6b86edd293d327caf3cdce7226554c00e7ff131
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:39:17 -0300

    add: Aula 6 do curso

---
+++ Aula06/Aula06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula05;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("----------VERIFICAR DE NÚMEROS PARES OU ÍMPARES------------");
+        Console.WriteLine("Digite um número:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int result = number % 2;
+
+        if (result == 0)
+        {
+            Console.WriteLine("O número " + number + " é par.");
+        }
+        else
+        {
+            Console.WriteLine("O número " + number + " é ímpar.");
+        }
+    }
+}
+
+
