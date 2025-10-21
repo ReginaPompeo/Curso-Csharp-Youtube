62799ca1af848f36eca3fc29c2372e188ee74d0f
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:38:31 -0300

    add: Aula 5 do curso

---
+++ Aula05/Aula05/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Aula05;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===========CALCULADORA=============");
+        Console.WriteLine("Digite o valor do número 1:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o valor do número 2:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        //int number1 = 10;
+        //int number2 = 20;
+
+        int sum = number1 + number2;
+        int subtraction = number2 - number1;
+        int multiplication = number1 * number2;
+        int division = number2 / number1;
+        //Module é o resto da divisão 
+        int module = number2 % number1;
+
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("O valor da soma é: " + sum);
+        Console.WriteLine("O valor da subtração é: " + subtraction);
+        Console.WriteLine("O valor da multiplicação é: " + multiplication);
+        Console.WriteLine("O valor da divisão é: " + division);
+        Console.WriteLine("O resto da divisão é: " + module);
+    }
+}
