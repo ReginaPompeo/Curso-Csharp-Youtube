01489de4d18ecb0b19768b5efbec2e5ba07b24dc
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:34:30 -0300

    add: Aula 1 do curso

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Aula 1
+namespace Aula01;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Aula 0
+        Console.WriteLine("Hello World");
+
+        Console.WriteLine("Iniciando em C#");
+    }
+}
+
