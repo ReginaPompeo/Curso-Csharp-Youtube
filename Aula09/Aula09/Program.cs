15c1e1153c8ed1bf47b54b62b01acc9ee6d2cbe2
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-20 21:42:51 -0300

    add: Aula 9 do curso

---
+++ Aula09/Aula09/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Aula09;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===== Operadores lógicos =====");
+
+        bool isLogged = true; // Verificar se o usuário está logado
+        bool hasAdminAcces = false; // Verificar se o usuário tem acesso de admin
+
+        Console.WriteLine("\nInformações do usuário:");
+        Console.WriteLine("Usuário logado: " + isLogged);
+        Console.WriteLine("Acesso ao painel de administrador: " + hasAdminAcces);
+
+        Console.WriteLine("\nPermissões: ");
+
+        //Operador (||) - Ou
+        if (isLogged || hasAdminAcces) // está dizendo (isLogged == true || hasAdminAcces == true)
+        {
+            Console.WriteLine("Acesso ao sistema concedido.");
+        }
+        else
+        {
+            Console.WriteLine("Acesso ao sistema não concedido.");
+        }
+
+        //Operador (&&) - E
+        if (isLogged && hasAdminAcces) // está dizendo (isLogged == true && hasAdminAcces == true)
+        {
+            Console.WriteLine("Acesso ao painel de admin conecedido.");
+        }
+        else
+        {
+            Console.WriteLine("Acesso ao painel de admin negado.");
+        }
+
+        //Operador (!) - "Não..."
+        if (!isLogged) // Ao contrário de true == false
+        {
+            Console.WriteLine("Usuário não está logado.");
+        }
+        else
+        {
+            Console.WriteLine("Usuário está logado.");
+        }
+    }
+
+}
