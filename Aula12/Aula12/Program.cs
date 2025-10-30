0a14d4b497e74b48bf11fdc49ae92ab65af27211
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-10-30 13:35:54 -0300

    add: Upload aula12 file

---
+++ Aula12/Aula12/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Exemplo 1
+        //int i = 0;
+
+        //while (i <= 5)
+        //{
+        //    Console.WriteLine(i);
+        //    i++;
+        //}
+
+        //Exemplo 2
+        double budget = 100;
+
+        while (budget > 0)
+        {
+            Console.WriteLine(budget);
+            //Realizando a operação de diminuir
+            budget--;
+        }
+
+        Console.WriteLine("Saldo insuficiente");
+    }
+}
